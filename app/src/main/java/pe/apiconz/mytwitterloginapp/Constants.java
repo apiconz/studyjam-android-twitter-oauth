c76e920e904eb6ed0f69679528850cdcb21f7ed5
Armando <apiconz@gmail.com>
2015-04-04 19:00:48 -0500

    integracion con Fabric.io

---
+++ app/src/main/java/pe/apiconz/mytwitterloginapp/Constants.java
@@ -0,0 +1,11 @@
+package pe.apiconz.mytwitterloginapp;
+
+/**
+ * Created by Indra on 04/04/2015.
+ */
+public class Constants {
+    //copied key and secret from fabric
+    public static final String TWITTER_KEY = "";
+    public static final String TWITTER_SECRET = "";
+
+}
